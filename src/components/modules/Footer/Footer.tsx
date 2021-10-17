ad49c8556b09080da8f4f025149052a5e71d5227
minusVoid <68424763+minusvoid@users.noreply.github.com>
2021-10-17 11:32:19 +0900

    PR#20 修正：フォントサイズ・位置・リンク

--- src/components/modules/Footer/Footer.tsx
+++ src/components/modules/Footer/Footer.tsx
@@ -3,52 +3,44 @@ import Link from "next/link";
 import clsx from "clsx";
 import SNSButtons from "../SNSButtons";
 
-const pageURL = {
-  top: "/",
-  about: "#",
-  company: "#",
-  contact: "#",
-  policy: "#",
-};
+const links = [
+  {
+    pagePath: "/",
+    description: "トップページ",
+  },
+  {
+    pagePath: "/about",
+    description: "オンライン開催について",
+  },
+  {
+    pagePath: "/company",
+    description: "協賛企業一覧",
+  },
+  {
+    pagePath: "/contact",
+    description: "お問い合わせ",
+  },
+  {
+    pagePath: "/policy",
+    description: "サイトポリシー",
+  },
+];
 
 const Footer: VFC = () => {
   return (
-    <footer
-      className={clsx(
-        "bg-k-navy-black",
-        "text-white",
-        ["text-[1.125rem]", "px-[10%]", "pt-[10%]", "pb-[130px]"],
-        ["k-lg:text-[1.35rem]", "k-lg:py-[5%]", "k-lg:pl-[7%]", "k-lg:pr-[20%]"]
-      )}
-    >
+    <footer className="mb-[104px] k-lg:mb-0 w-full k-lg:w-[calc(100%-20vw)] bg-k-navy-black text-white text-[1.8rem] px-[10%] pt-[10%] pb-[130px] k-lg:py-[5%] k-lg:pl-[7%] k-lg:pr-[20%] absolute bottom-0 k-lg:right-0">
       <div>
         <SNSButtons className="mb-[20px]" />
         <ul className="space-y-[10px]">
-          <li>
-            <Link href={pageURL.top}>
-              <a>トップページ</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={pageURL.about}>
-              <a>オンライン開催について</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={pageURL.company}>
-              <a>協賛企業一覧</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={pageURL.contact}>
-              <a>お問い合わせ</a>
-            </Link>
-          </li>
-          <li>
-            <Link href={pageURL.policy}>
-              <a>サイトポリシー</a>
-            </Link>
-          </li>
+          {links.map(({ pagePath, description }) => {
+            return (
+              <li key={pagePath}>
+                <Link href={pagePath}>
+                  <a>{description}</a>
+                </Link>
+              </li>
+            );
+          })}
         </ul>
       </div>
     </footer>
