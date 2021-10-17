deeea0cd4f6084d686f6edcccb7e8fc4f65a34ba
minusVoid <68424763+minusvoid@users.noreply.github.com>
2021-10-17 21:33:35 +0900

    背景以外完成

--- src/components/templates/DefaultLayout.tsx
+++ src/components/templates/DefaultLayout.tsx
@@ -1,6 +1,7 @@
 import type { ReactNode, VFC } from "react";
 import Header from "src/components/modules/Header/Header";
 import Footer from "src/components/modules/Footer/Footer";
+import EventDay from "src/components/modules/EventDay";
 
 type Props = {
   children: ReactNode;
@@ -10,6 +11,7 @@ const DefaultLayout: VFC<Props> = (props) => {
   return (
     <>
       <Header />
+      <EventDay className="fixed top-0 right-0" />
       {props.children}
       <Footer />
     </>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,19 @@
+import clsx from "clsx";
 import type { NextPage } from "next";
+import Link from "next/link";
 import Head from "next/head";
+import Subtitle from "src/components/modules/Subtitle";
+
+const announce = [
+  {
+    date: "11月06日",
+    detail: "こうがく祭開催予定",
+  },
+  {
+    date: "10月06日",
+    detail: "開催1ヵ月前",
+  },
+];
 
 const Home: NextPage = () => {
   return (
@@ -7,8 +21,54 @@ const Home: NextPage = () => {
       <Head>
         <title>2020年こうがく祭公式HP | 茨城大学</title>
       </Head>
-      <main>
-        <p>pages:index</p>
+      <main className="k-lg:w-[calc(100%-20vw)] ml-auto">
+        {/* <div className={clsx("flex items-center justify-center")}>
+          <div
+            className={clsx(
+              "w-full h-[737px] k-sm:h-[1366px] k-lg:h-[1080px] flex",
+              "bg-center bg-cover bg-origin-content box-border bg-no-repeat",
+              "bg-pre-sm k-sm:bg-pre-md k-lg:bg-pre-lg text-center"
+            )}
+          >
+            <h1 className="items-center">
+              <img src="/title.png" className="border-none" />
+            </h1>
+          </div>
+        </div> */}
+        <article
+          className={clsx(
+            "p-[10%] k-lg:py-[5%] k-lg:pl-[7%] k-lg:pr-[20%] k-lg:min-h-[500px]",
+            "space-y-[50px] text-[1.8rem] bg-white items-center"
+          )}
+        >
+          <section>
+            <Subtitle text="こうがく祭とは" />
+            <p>こうがく祭は毎年11月に開催される茨城大学工学部の学園祭です。</p>
+            <p>
+              工学部ならではの企画や研究室公開などがあり、毎年一般の方や学生の方含め多くの方にご来場していただいております。
+            </p>
+            <p>
+              詳しくは
+              <Link href="/about">
+                <a>オンライン開催について</a>
+              </Link>
+              をご覧ください。
+            </p>
+          </section>
+          <section>
+            <Subtitle text="お知らせ" />
+            <ul>
+              {announce.map(({ date, detail }) => {
+                return (
+                  <li className="h-[38px] leading-[38px]" key={date}>
+                    <span className="w-[150px] mr-[30px]">{date}</span>
+                    <span>{detail}</span>
+                  </li>
+                );
+              })}
+            </ul>
+          </section>
+        </article>
       </main>
     </>
   );
