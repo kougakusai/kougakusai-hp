0ea36af0f4ae3e069a6396dddde75f5812e49fa3
minusVoid <68424763+minusvoid@users.noreply.github.com>
2021-10-31 23:37:44 +0900

    企業名表示

--- src/pages/company.tsx
+++ src/pages/company.tsx
@@ -3,7 +3,74 @@ import Head from "next/head";
 import ArticleLayout from "src/components/templates/ArticleLayout";
 import Subtitle from "src/components/modules/Subtitle";
 
-const companies = [];
+const companies = [
+  {
+    name: "アイ・イー・シー(株)",
+    ruby: "あいいーしー",
+    url: "",
+    img: "https://drive.google.com/uc?id=1ah7B1OdoF2vT0ZgzlRrgZOajJtju6PoY",
+  },
+  {
+    name: "イガラシ綜業株式会社",
+    ruby: "いがらしそうぎょうかぶしきがいしゃ",
+    url: "https://www.igarashisogyo.com/",
+    img: "https://drive.google.com/uc?id=1L0vZ1DRx4g80ptHqLvmjVTFnv_1RtBkq",
+  },
+  {
+    name: "有)エイトプランニングオフィス",
+    ruby: "えいとぷらんにんぐおふぃす",
+    url: "",
+    img: "https://drive.google.com/uc?id=1ReL-sxlkzO_xiQ-myIxMaN0i2ib0TcAh",
+  },
+  {
+    name: "小野不動産",
+    ruby: "おのふどうさん",
+    url: "",
+    img: "https://drive.google.com/uc?id=1ga13D_0AZKWXN52ucdH0OO3f178AOGtk",
+  },
+  {
+    name: "香陵住販株式会社　日立支店",
+    ruby: "こうりょうじゅうはんかぶしきがいしゃひたちしてん",
+    url: "",
+    img: "https://drive.google.com/uc?id=1sb4j-AEF9mXYTY770NKFDyRgkjUQ21Ti",
+  },
+  {
+    name: "サイクルショップイマイ",
+    ruby: "さいくるしょっぷいまい",
+    url: "",
+    img: "https://drive.google.com/uc?id=17pxv2xFOA1IaWoweWzPN5dxxT-ar_5D5",
+  },
+  {
+    name: "社会医療法人愛宣会ひたち医療センター",
+    ruby: "しゃかいいりょうほうじんあいせんかいひたちいりょうせんたー",
+    url: "",
+    img: "https://drive.google.com/uc?id=1UeBnBR2bnrFnvspChFDLTvh35aLWR6KD",
+  },
+  {
+    name: "電鉄タクシー株式会社",
+    ruby: "でんてつたくしーかぶしきがいしゃ",
+    url: "",
+    img: "https://drive.google.com/uc?id=1AhMvVLgJ0CFPXd60YWo_3xjGtv-LVqD5",
+  },
+  {
+    name: "はくせん",
+    ruby: "はくせん",
+    url: "",
+    img: "https://drive.google.com/uc?id=12N_Ye8r1Ngs8wnlhY-thIaZ9vFvsUTEk",
+  },
+  {
+    name: "モモタ電気サービス",
+    ruby: "ももたでんきさーびす",
+    url: "",
+    img: "https://drive.google.com/uc?id=1Le9WjwKOSwcBSfKvK-PeTv4VEcbdvXp5",
+  },
+  {
+    name: "よしだ写真館",
+    ruby: "よしだしゃしんかん",
+    url: "http://yoshida-shashinkan.jp/",
+    img: "https://drive.google.com/uc?id=1lBCzXTqkXKuWca8yERorunk4y7png9-S",
+  },
+];
 
 function Company() {
   return (
@@ -13,30 +80,41 @@ function Company() {
       </Head>
       <article className="text-[1.8rem]">
         <div className="mb-[50px]">
-          <Subtitle text="ご協賛いただいた企業一覧（敬称略・順不同）" />
+          <Subtitle text="ご協賛いただいた企業一覧（敬称略・50音順）" />
           <p className="p-[6px]">
             下線、色付きのご協賛企業名はクリック／タップしていただくと、各企業のホームページへジャンプします。
           </p>
         </div>
-        <ul className="list-disc pl-[2rem] space-y-[20px]">
-          {companies.map(({ name, pageUrl }) => {
-            if (pageUrl) {
+        <ul className="pl-[2rem] grid grid-cols-1 k-lg:grid-cols-1 gap-8 divide-y-2">
+          {companies
+            .sort((a, b) => {
+              return a.ruby > b.ruby ? 1 : -1;
+            })
+            .map(({ name, url, img }) => {
               return (
-                <li key={name}>
-                  <a
-                    href={pageUrl}
-                    target="_blank"
-                    rel="noreferrer"
-                    className="underline text-k-blue-dark"
-                  >
-                    {name}
-                  </a>
+                <li key={name} className="p-4 space-y-6">
+                  {url ? (
+                    <a
+                      href={url}
+                      target="_blank"
+                      rel="noreferrer"
+                      className="mb-2 underline text-k-blue-dark"
+                    >
+                      {name}
+                    </a>
+                  ) : (
+                    <span className="mb-2">{name}</span>
+                  )}
+                  {img && (
+                    <img
+                      src={img}
+                      alt={name + "広告"}
+                      className="w-2/3 m-auto"
+                    />
+                  )}
                 </li>
               );
-            } else {
-              return <li key={name}>{name}</li>;
-            }
-          })}
+            })}
         </ul>
       </article>
     </>
