f76e9457d35a70f913715cb1d2f49bc4e226bd28
minusVoid <68424763+minusvoid@users.noreply.github.com>
2021-11-03 22:54:27 +0900

    画像パス修正

--- src/components/modules/TitleBand/VTitleBand.tsx
+++ src/components/modules/TitleBand/VTitleBand.tsx
@@ -16,7 +16,7 @@ const VTitleBand: VFC<Props> = (props) => {
     >
       <div
         className="relative w-[1080px] h-[110px] origin-bottom-left transform rotate-90"
-        style={{ backgroundImage: "url('./rect.svg')" }}
+        style={{ backgroundImage: "url('/rect.svg')" }}
       >
         <div className="absolute top-[40%] left-[20vh] text-white text-[3.5rem] font-semibold">
           {props.text}
