5c7c79f015179dfdb81f1e006c295a6a21e2709e
MTea <53252847+ujimattya@users.noreply.github.com>
2021-11-04 12:46:01 +0900

    協賛企業名修正 (#60)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,14 @@
-import type { ReactElement } from "react";
-import Head from "next/head";
-import Link from "next/link";
-import ArticleLayout from "src/components/templates/ArticleLayout";
-import Subtitle from "src/components/modules/Subtitle";
+import type { ReactElement } from 'react';
+import Head from 'next/head';
+import Link from 'next/link';
+import ArticleLayout from 'src/components/templates/ArticleLayout';
+import Subtitle from 'src/components/modules/Subtitle';
 
 export default function Custom404() {
   return (
     <>
       <Head>
-        <title>2020年こうがく祭公式HP | 茨城大学</title>
+        <title>2021年こうがく祭公式HP | 茨城大学</title>
       </Head>
       <article className="relative top-[calc(-1.2rem-26px)] k-lg:top-[calc(-1.2rem-27px)]">
         <Subtitle text="404 NotFound" />
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,12 +1,12 @@
-import type { ReactElement } from "react";
-import Head from "next/head";
-import ArticleLayout from "src/components/templates/ArticleLayout";
+import type { ReactElement } from 'react';
+import Head from 'next/head';
+import ArticleLayout from 'src/components/templates/ArticleLayout';
 
 export default function About() {
   return (
     <>
       <Head>
-        <title>2020年こうがく祭公式HP | 茨城大学</title>
+        <title>2021年こうがく祭公式HP | 茨城大学</title>
       </Head>
       <article className="relative top-[calc(-1.2rem-26px)] k-lg:top-[calc(-1.2rem-27px)]">
         <p className="p-2 mb-[50px] text-[1.5rem] k-lg:text-[1.8rem]">
