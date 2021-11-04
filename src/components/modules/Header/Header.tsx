bf9360ba61e65fbe99f1a3d97f2933238998e322
minusVoid <68424763+minusvoid@users.noreply.github.com>
2021-11-04 17:53:09 +0900

    企画ページ周り調整 #62追加

--- src/components/modules/Header/Header.tsx
+++ src/components/modules/Header/Header.tsx
@@ -41,12 +41,28 @@ const Header: VFC = () => {
           />
         </div>
         <div className="w-full px-[15%] py-[10px]">
-          <div className="w-[44px]">
-            <Link href="/">
-              <a onClick={() => setIsExpand(false)}>
-                <img src={`${prefix}/home.svg`} alt="TOPへ" width="44px" />
-              </a>
-            </Link>
+          <div className="flex justify-between text-white text-center">
+            <div>
+              <Link href="/">
+                <a onClick={() => setIsExpand(false)} className="w-[44px]">
+                  <img src={`${prefix}/home.svg`} alt="TOPへ" width="44px" />
+                  <span>Home</span>
+                </a>
+              </Link>
+            </div>
+            <div>
+              <Link href="/event">
+                <a onClick={() => setIsExpand(false)} className="w-[44px]">
+                  <img
+                    src={`${prefix}/event.svg`}
+                    alt="企画ページへ"
+                    width="44px"
+                    height="44px"
+                  />
+                  <span>Event</span>
+                </a>
+              </Link>
+            </div>
           </div>
         </div>
       </div>
