ee014438ea62c873475d386dbdb10357a141317e
kazuhiro-f <kz7260wanderer@gmail.com>
2021-11-05 22:24:13 +0900

    #67 視聴用リンクのボタン設置

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -22,16 +22,35 @@ const announcement = [
 const headline = (
   <span
     className={clsx(
-      "mt-[-35%] k-sm:mt-[-20%] k-lg:mt-[-18%]",
-      "pl-[25vw] k-sm:pl-[6vw] k-lg:pl-[3vw]",
-      "w-10/12 k-sm:w-3/4 k-lg:w-full float-left",
+      "mt-[-35%] k-sm:mt-[-25%] k-lg:mt-[-28%]",
+      "pl-[27vw] k-sm:pl-[8vw] k-lg:pl-[3vw]",
+      "k-lg:w-full float-left",
       "text-[8vw] k-sm:text-[6vw] k-lg:text-[2.8vw] text-center leading-normal tracking-widest",
       "text-yellow-200 brightness-125 blur-[0.8px] font-bold"
     )}
   >
     オンライン開催
     <br />
-    11月06日(土) 10:00配信開始
+    <div>
+      <a
+        href="#"
+        className="inline-flex items-top px-[30px] py-[10px] bg-[#fe0e73] text-white rounded-lg"
+      >
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="w-[8vw] k-sm:w-[6vw] k-lg:w-[2.8vw]"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zM9.555 7.168A1 1 0 008 8v4a1 1 0 001.555.832l3-2a1 1 0 000-1.664l-3-2z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <span>配信を見る</span>
+      </a>
+    </div>
   </span>
 );
 
@@ -64,7 +83,11 @@ export default function Home() {
             "k-lg:top-[5vh] k-lg:left-[calc(50%-13vw)] m-auto"
           )}
         >
-          <img src={`${prefix}/title.png`} alt="こうがく祭" className="w-full k-sm:w-[58vw] k-lg:w-[40vw]" />
+          <img
+            src={`${prefix}/title.png`}
+            alt="こうがく祭"
+            className="w-full k-sm:w-[58vw] k-lg:w-[40vw]"
+          />
           {headline}
         </h1>
         <article
@@ -82,7 +105,9 @@ export default function Home() {
             <p>
               今年度はオンラインでの開催となりました。詳しくは
               <Link href="/about">
-                <a className="underline text-k-blue-dark cursor-pointer">オンライン開催について</a>
+                <a className="underline text-k-blue-dark cursor-pointer">
+                  オンライン開催について
+                </a>
               </Link>
               をご覧ください。
             </p>
